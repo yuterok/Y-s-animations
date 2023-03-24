4494b39f30c0f337bdf6b223e7ac672cb90143af
Julia Tereschenko <yuterok@gmail.com>
2023-03-24 03:33:05 +0300

    scroll с костылём, потом переделаю с jquery
    scroll + logo fixed

---
+++ app.js
@@ -0,0 +1,20 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors){
+    anchor.addEventListener("click", function(event){
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        console.log(blockID)
+        if (blockID == '#skills'){
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: "smooth",
+                block: "end"
+            })
+        } else {
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            })
+        }
+    })
+}
